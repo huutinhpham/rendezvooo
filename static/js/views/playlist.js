8ca94a570a51378ed5a17ba5dc97ae99a14e7e31
Huu Pham <huutinh@berkeley.edu>
2017-12-28 17:10:12 -0800

    added song thumbnails and titles to playlist

--- static/js/views/playlist.js
+++ static/js/views/playlist.js
@@ -4,6 +4,7 @@ var playlist = {
 		this.playlist = controller.getPlaylist();
 		this.renderPlayer();
 		this.renderSong(this.playlist[0].url);
+		this.renderPlaylist();
 	},
 
 	renderPlayer: function() {
@@ -14,15 +15,36 @@ var playlist = {
 	},
 
 	renderPlaylist: function() {
+		var playlistContainer = document.createElement('div');
+		playlistContainer.id = 'playlist-container'
+
+		for (var i = 1; i < this.playlist.length; i++) {
+			var songView = this.renderSong(this.playlist[i].url);
+			playlistContainer.append(songView);
+		}
+		$('body').append(playlistContainer);
 
 	},
 
 	renderSong: function(songId) {
 		var key = 'AIzaSyB-fC4XB3x1GXFoNY-4yTYzatwd4iEYX3M'
-		console.log(songId);
+		var songContainer = document.createElement('div');
+		songContainer.className = 'song-container';
 		$.getJSON('https://www.googleapis.com/youtube/v3/videos?key='+key+'&part=snippet&id='+songId, function(data) {
-			console.log(data.items[0].snippet.title);
+
+			var thumbnail = document.createElement('img');
+			thumbnail.className = 'thumbnail';
+			thumbnail.src = data.items[0].snippet.thumbnails.medium.url;
+
+			var songTitle = document.createElement('p');
+			songTitle.className = 'song-title';
+			songTitle.innerHTML = data.items[0].snippet.title;
+
+			songContainer.append(thumbnail);
+			songContainer.append(songTitle);
 		});
+
+		return songContainer;
 	}
 
 }
