52a0f531f5a27f7ba1434df3ba6add176b3243e2
Huu Pham <huutinh@berkeley.edu>
2018-01-03 16:34:07 -0800

    able to give POST request for new songs, code need to be refactored

--- __init__.py
+++ __init__.py
@@ -24,12 +24,12 @@ def generate_playlist():
 			c, conn = connection()
 
 			while pid == '':
-				pid = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for i in range(8));
-				server_pid = c.execute("SELECT * FROM Playlists WHERE pid = (%s)", (pid,))
-				if server_pid is not None:
+				pid = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for i in range(8))
+				c.execute("SELECT * FROM Playlist WHERE pid = (%s)", (pid,))
+				if c.fetchone() is not None:
 					pid = ''
 
-			c.execute("INSERT INTO Playlists (pid, playlist_pw, email, dirty) VALUES (%s, %s, %s, %s)", (pid, playlist_pw, email, False))
+			c.execute("INSERT INTO Playlist (pid, playlist_pw, email, dirty) VALUES (%s, %s, %s, %s)", (pid, playlist_pw, email, False))
 			conn.commit()
 			conn.close
 			session['admin'] = True
@@ -44,6 +44,31 @@ def generate_playlist():
 
 @app.route('/playlist/', methods=['GET', 'POST'])
 def playlist():
+	error = ''
+	try:
+		c, conn = connection()
+		if request.method == 'POST':
+			ytId = bleach.clean(request.form['yt_id'])
+			pid = '12345678'
+
+			c.execute("SELECT * FROM Playlist WHERE pid = (%s)", (pid,))
+			dbpid = 'hello'
+			c.execute("SELECT * FROM Song WHERE pid = (%s) AND ytid = (%s)", (pid, ytId))
+			song = c.fetchone()
+
+			if dbpid is None:
+				error = 'That Playlist does not Exist, please create a Playlist First'
+			elif song is not None:
+				error = 'That song has already been requested'
+			elif len(ytId) != 11:
+				error = 'please enter a valid YouTube URL'
+			else:
+				c.execute("INSERT INTO Song (pid, ytid, vote) VALUES (%s, %s, %s)", (pid, ytId, 0))
+				conn.commit()
+		conn.close()
+		return render_template("playlist.html")
+	except Exception as e:
+		flash(e)
 	return render_template("playlist.html")
 
 @app.errorhandler(500)
